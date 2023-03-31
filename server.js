d9665344ebb7f7d8b2ace1456a6725f42e995dd7
Dwayne Foreman <dwayneforemanjr@yahoo.com>
2023-03-30 21:49:24 -0400

    new commit

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const app = express();
 
 app.use(express.static(`${__dirname}/public`));
 
-app.listen(4100, () => console.log(`LFGGGG!!`));
+app.listen(4100, () => console.log(`Up and running on 4100`));
